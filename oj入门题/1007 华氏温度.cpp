5902cff10db19f5bda8e185768062e0c0458054c
Visua1 <114505119+XD0111@users.noreply.github.com>
2023-04-09 11:45:00 +0800

    Add files via upload

---
+++ oj入门题/1007 华氏温度.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+ 	double f;
+ 	while(cin>>f)
+ 	{
+ 		double c;
+ 		c=(double)(f-32)*5/9;
+ 		printf("%.2fF = %.2fC\n",f,c);
+	}
+ 	return 0;
+}
